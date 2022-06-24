a793e4116aa80c424f5e655af81817faf0fb1ea0
CamanuCow <106633425+CamanuCow@users.noreply.github.com>
2022-06-25 01:20:02 +0300

    Add files via upload

--- rollerjs.js
+++ rollerjs.js
@@ -12,20 +12,33 @@ function getRandomIntInclusive(min, max) {
     if (Jopa == 1) {
         img.src = "Res/Side1D6.png";
     }
-    if (Jopa == 2) {
+    else if (Jopa == 2) {
         img.src = "Res/Side2D6.png";
     }
-    if (Jopa == 3) {
+    else if (Jopa == 3) {
         img.src = "Res/Side3D6.png";
     }
-    if (Jopa == 4) {
+    else if (Jopa == 4) {
         img.src = "Res/Side4D6.png";
     }
-    if (Jopa == 5) {
+    else if (Jopa == 5) {
         img.src = "Res/Side5D6.png";
     }
-    if (Jopa == 6) {
+    else if (Jopa == 6) {
         img.src = "Res/Side6D6.png";
     }
     return Jopa;
+    
+}
+
+function updateOp(val) {
+    var numero = parseInt(getRandomIntInclusive(1, 6));
+    if (val === "+") {
+
+        document.getElementById("result").innerHTML = "Your result: "+ (10 + numero);
+
+    }
+    else if (val === "*"){
+        document.getElementById("result").innerHTML = "Your result: "+ (numero * 10);
+    }
 }
